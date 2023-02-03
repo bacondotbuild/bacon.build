e862625295d0598e0e62f8bc3f85a9cc4d883ad7
bacon.build <bacondotbuild@gmail.com>
2023-02-02 17:04:31 -0800

    update styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -16,10 +16,14 @@ const Home: NextPage = () => {
   return (
     <Page>
       <Main className='flex flex-col p-4'>
-        <div className='flex flex-grow flex-col items-center justify-center space-y-4'>
+        <div className='flex flex-grow flex-col items-center justify-center space-y-3'>
           <Title>bacon.build</Title>
-          <p>hi my name is bacon</p>
-          <p>and i build things.</p>
+          <p>
+            hi my name is <span className='text-cb-orange'>bacon</span>
+          </p>
+          <p>
+            and i <span className='text-cb-yellow'>build</span> things.
+          </p>
           <ul className='space-y-2'>
             {links
               .map(link => ({
@@ -31,7 +35,7 @@ const Home: NextPage = () => {
               .map(({ text, url }) => (
                 <li key={url}>
                   <a
-                    className='text-cb-pink'
+                    className='text-cb-pink hover:underline'
                     href={url}
                     target='_blank'
                     rel='noopener noreferrer'
